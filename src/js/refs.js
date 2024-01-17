f3390f2fdd19c5dbd14f569f0cc3030d6676bd8e
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2024-01-17 19:57:58 +0200

    delete loadMore

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,6 @@
 const refs = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
-  loadMore: document.querySelector('.show-load-more'),
   searchBtn: document.querySelector('.search-submit '),
   loader: document.querySelector('.loader'),
 };
