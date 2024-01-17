b502cf6827abb5b728fd474cd272a299738d3957
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2024-01-17 20:46:18 +0200

    add change

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
-// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createMarkup } from './markup';
 import { getImage } from './API';
 import { refs } from './refs';
@@ -12,18 +9,23 @@ import { refs } from './refs';
 let page = 0;
 let query = '';
 
+refs.loader.style.display = 'none';
+
+// SUBMIT FORM
 refs.form.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit(e) {
   e.preventDefault();
-
-  query = e.target.elements.searchQuery.value;
+  refs.loader.style.display = 'block';
+  query = e.currentTarget.searchQuery.value;
   page = 1;
+
+  // API PIXABAY
   getImage(query, page)
     .then(data => {
       if (data.total === 0) {
         page = 0;
         refs.searchBtn.disabled = true;
-
+        refs.loader.style.display = 'none';
         iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
@@ -31,9 +33,11 @@ function handleFormSubmit(e) {
         return;
       }
 
+      //   ADD MARKUP
+      refs.loader.style.display = 'none';
       refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-      // refs.loadMore.hidden = true;
-      // refs.loadMore.hidden = false;
+
+      // SIMPLELIGHTBOX
       const lightbox = new SimpleLightbox('.gallery a', {
         captions: true,
         captionsData: 'alt',
@@ -46,7 +50,7 @@ function handleFormSubmit(e) {
     })
 
     .catch(() => {
-      // refs.loadMore.hidden = true;
+      refs.loader.style.display = 'none';
       iziToast.error({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -59,74 +63,3 @@ function handleFormSubmit(e) {
 
   refs.gallery.innerHTML = '';
 }
-
-// !import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { createMarkup } from './markup';
-// import { getImage } from './API';
-// import { refs } from './refs';
-// let lightbox = '';
-// let page = 0;
-// let query = '';
-
-// refs.loader.style.display = 'none';
-
-// // SUBMIT FORM
-// refs.form.addEventListener('submit', handleFormSubmit);
-// function handleFormSubmit(e) {
-//   e.preventDefault();
-//   refs.loader.style.display = 'block';
-//   query = e.currentTarget.searchQuery.value;
-//   page = 1;
-
-//   // API PIXABAY
-//   getImage(query, page)
-//     .then(data => {
-//       if (data.total === 0) {
-//         page = 0;
-//         refs.searchBtn.disabled = true;
-//         refs.loader.style.display = 'none';
-//         iziToast.error({
-//           message:
-//             'Sorry, there are no images matching your search query. Please try again!',
-//         });
-//         return;
-//       }
-
-//       //   ADD MARKUP
-//       refs.loader.style.display = 'none';
-//       refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-
-//       // SIMPLELIGHTBOX
-//       lightbox = new SimpleLightbox('.gallery a', {
-//         captions: true,
-//         captionsData: 'alt',
-//         captionPosition: 'bottom',
-//         captionDeloy: 250,
-//       });
-
-//       lightbox.on('show.simplelightbox', function (e) {
-//         e.preventDefault();
-//       });
-//       // lightbox.refresh = function () {
-//       //   e.preventDefault();
-//       //   lightbox.destroy();
-//       // };
-//     })
-
-//     .catch(() => {
-//       refs.loader.style.display = 'none';
-//       iziToast.error({
-//         message:
-//           'Sorry, there are no images matching your search query. Please try again!',
-//       });
-//     })
-//     .finally(() => {
-//       refs.form.reset();
-//       refs.searchBtn.disabled = false;
-//     });
-
-//   refs.gallery.innerHTML = '';
-// }
